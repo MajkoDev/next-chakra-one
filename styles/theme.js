cc03bb3ff8b422e3f20fedc61f432e61ca76b672
Marian <hellomariandev@gmail.com>
2022-02-16 21:35:30 +0100

    import chakra

---
+++ styles/theme.js
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react'
+import {theme as chakraTheme} from '@chakra-ui/react'
+import {createBreakpoints} from '@chakra-ui/theme-tools'
+
+const fonts = {
+    ...chakraTheme.fonts,
+    
+}
+
+const overrides = {
+    ...chakraTheme,
+}
+
+const customTheme = extendTheme(overrides)
+
+export default customTheme
